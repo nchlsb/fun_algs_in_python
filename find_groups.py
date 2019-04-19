8e1cadac4f8a8e5badf35810b98f9834f69e32c0
tug360667 <tug36067@temple.edu>
2019-04-19 14:36:48 -0400

    Edge case: find_group for group size = 1

--- find_groups.py
+++ find_groups.py
@@ -7,18 +7,20 @@ def find_groups(arr, group_size):
     seen = {}
     groups = 0
 
-    for i in range(0, len(arr)):        
+    for i in range(0, len(arr)):         
         if seen.__contains__(arr[i]) == False:
-            seen[arr[i]] = 1
+            """
+            You wouldn't need a more complicated method to find groups of
+                size 1, but this hadnles it just in case
+            """           
+            if group_size != 1:
+                seen[arr[i]] = 1
+            else:
+                groups += 1
             
-        elif seen[arr[i]] + 1 == group_size:
-            del seen[arr[i]]   
-            groups += 1
-
-        # edge case: group_size = 1    
-        elif seen[arr[i]] + 1 > group_size:  
-            del seen[arr[i]]   
-            groups += 2            
+        elif seen[arr[i]] + 1 >= group_size:
+            del seen[arr[i]]
+            groups += 1                               
             
         else:
             seen[arr[i]] += 1
